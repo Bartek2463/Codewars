da791814533637e3f6553b011db5d37e0926a8b3
bszybiak <szybaikbartlomiej@gmail.com>
2022-11-25 00:24:58 +0100

    dna deoxyribonucleic acid repeat

---
+++ src/CodewarsMain.java
@@ -0,0 +1,7 @@
+public class CodewarsMain {
+    public static void main(String[] args) {
+        SimpleTask simpleTask = new SimpleTask();
+
+        System.out.println(simpleTask.makeComplement("GCAT"));
+    }
+}
--- src/SimpleTask.java
+++ src/SimpleTask.java
@@ -1,3 +1,12 @@
 public class SimpleTask {
 
+    public String makeComplement(String dna) {
+
+        return dna.replace("A", "Z")
+                .replace("T", "A")
+                .replace("Z", "T")
+                .replace("G", "Z")
+                .replace("C", "G")
+                .replace("Z", "C");
+    }
 }
