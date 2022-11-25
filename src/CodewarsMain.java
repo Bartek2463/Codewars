7985be415847bd8ffcc8723164382f77acfdb2e1
bszybiak <szybaikbartlomiej@gmail.com>
2022-11-25 01:24:21 +0100

    My solution sum without highest and lowest number

--- src/CodewarsMain.java
+++ src/CodewarsMain.java
@@ -2,6 +2,7 @@ public class CodewarsMain {
     public static void main(String[] args) {
         SimpleTask simpleTask = new SimpleTask();
 
-        System.out.println(simpleTask.makeComplement("GCAT"));
+//        System.out.println(simpleTask.makeComplement("GCAT"));
+        System.out.println(simpleTask.sum(new int[]{19}));
     }
 }
--- src/SimpleTask.java
+++ src/SimpleTask.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class SimpleTask {
 
     public String makeComplement(String dna) {
@@ -9,4 +12,17 @@ public class SimpleTask {
                 .replace("C", "G")
                 .replace("Z", "C");
     }
+
+    public int sum(int[]numbers){
+        int sum = 0;
+        if (numbers==null||numbers.length==1||numbers.length==0){
+            sum=0;
+        }else {
+            int min = Arrays.stream(numbers).min().getAsInt();
+            int max = Arrays.stream(numbers).max().getAsInt();
+            sum = Arrays.stream(numbers).sum() - min - max;
+        }
+        System.out.println(sum);
+            return sum;
+        }
 }
