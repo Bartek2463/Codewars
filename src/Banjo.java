800fae7693f84fd84fd747decfadfc2a8bf689fc
bszybiak <szybaikbartlomiej@gmail.com>
2023-01-04 22:11:09 +0100

    Regular Expressions

--- src/Banjo.java
+++ src/Banjo.java
@@ -1,15 +1,28 @@
+import java.util.Arrays;
+import java.util.regex.Matcher;
+
 public class Banjo {
     public static void main(String[] args) {
 
-areYouPlayingBanjo("ricy");
+     //   areYouPlayingBanjo("ricy");
+fakeBin("9123456789");
 
     }
-    public static String areYouPlayingBanjo(String name){
 
-        if (name.startsWith("R")||name.startsWith("r")){
-            return name+" plays banjo";
-        }else {
-            return name+" does not play banjo";
-        }
+    public static String areYouPlayingBanjo(String name) {
+
+        return name.startsWith("R") || name.startsWith("r") ? name + "plays banjo" : name + "does not play banjo";
     }
+
+    public static String[] stringToArray(String s){
+
+        return  s.split(" ");
+
+    }
+    public static String fakeBin(String numberString){
+
+        String s = numberString.replaceAll("[0-4]", "0");
+        return s.replaceAll("[^0]", "1");
+    }
+
 }
