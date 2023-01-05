fb17001c712aa2e91b7017b61537bee822912407
bszybiak <szybaikbartlomiej@gmail.com>
2023-01-05 01:48:49 +0100

    Refactor Code

--- src/FristNonConsecutive.java
+++ src/FristNonConsecutive.java
@@ -4,13 +4,10 @@ public class FristNonConsecutive {
     }
 
     static Integer find(final int[] array) {
-        int findnumber = 0;
-        for (int i = 0; i < array.length - 1; i++) {
-            findnumber = array[i + 1] - array[i];
-            if (findnumber > 1) {
-                return array[i + 1];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] - array[i - 1] != 1) {
+                return array[i];
             }
-
         }
         return null;
     }
