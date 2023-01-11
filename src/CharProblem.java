9ee66f258f72106dfb6befd9c6cf93f423845d49
bszybiak <szybaikbartlomiej@gmail.com>
2023-01-11 22:52:22 +0100

    Stray algoritm simple task

--- src/CharProblem.java
+++ src/CharProblem.java
@@ -1,7 +1,3 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.IntStream;
-
 public class CharProblem {
 
     public static int howOld(final String herOld){
@@ -13,6 +9,14 @@ public class CharProblem {
     public static String correct(String string){
 
        return string.replace("5", "S").replace("0", "O").replace("1", "I");
+    }
+
+    public int stray(int[]numbers){
+
+        for (Integer number: numbers) {
+            System.out.println(number);
+        }
+        return 0;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,12 @@ public class Main {
         Fundamentals fundamentals = new Fundamentals();
 //        System.out.println(fundamentals.mexicanWave("hello"));
          // fundamentals.wave("hallo");
-    CharProblem.howOld("5 years old");
-    CharProblem.correct("1F-RUDYARD K1PL1NG");
+//        CharProblem.howOld("5 years old");
+//        CharProblem.correct("1F-RUDYARD K1PL1NG");
+
+    CharProblem charProblem = new CharProblem();
+    charProblem.stray(new int[]{17, 17, 3, 17, 17, 17, 17});
+
     }
 
 
